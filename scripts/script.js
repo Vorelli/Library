60a6c516c773d4ffd30a7fe05c86538f325fe4bf
Natale Toscano <kickalliance195@gmail.com>
2019-11-20 05:26:12 -0500

    Add hamburger menu and add menu; Set up book notes
    Did a lot this commit. Added functioning menus. Each have similar looks,
    but they're going to have different things in them. For now, I don't
    remember what the hamburger menu is going to be used for, but I know I'm
    planning on using the current add menu to edit additions in the future.
    
    Clearly, I'm pleased with results and am looking forward to progress
    with this website.
    
    Planned:
        Ability to edit books added to the 'library'.
        Persistent storage functionality.
        Ability to remove books from the 'library'.

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,14 @@ let bookList = body.querySelector('#bookList');
 let titleBar = body.querySelector('#titleBar');
 let template = document.querySelector('.bookListItem');
 template.parentElement.removeChild(template);
+let menu = document.querySelector('#menu');
+let addBook = document.querySelector('#addBook');
 
-function Book(title, author, numPages, read) {
+function Book(title, author, numPages, notes, read) {
     this.title = title;
     this.author = author;
     this.numPages = numPages;
+    this.notes = notes;
     this.read = read;
 }
 
@@ -16,8 +19,8 @@ Book.prototype.toString = function() {
     return this.title + ' by ' + this.author + ': ' + this.numPages + ' pages long. Read: ' + this.read;
 }
 
-function addBookToLibrary(title, author, numPages, read) {
-    myLibrary.push(new Book(title,author, numPages, read));
+function addBookToLibrary(title, author, numPages, notes, read) {
+    myLibrary.push(new Book(title,author, numPages, notes, read));
 }
 
 function render() {
@@ -34,9 +37,11 @@ function setUpTemplate(book, template, bottomBorder) {
     let temp = template.cloneNode(true);
     temp.querySelector('#title').textContent = book.title;
     temp.querySelector('#author').textContent = "by " + book.author;
+    temp.querySelector('#desc').textContent = book.notes;
     temp.querySelector('#read').checked = book.read;
     temp.querySelector('#read').disabled = true;
-    temp.querySelector('#amazonLink').parentElement.href = 'https://www.amazon.com/s?k=' + book.title.replace(' ','+') + '+' + book.author.replace(' ', '+');
+    let tempTitle = book.title.indexOf(' ') != -1 ? book.title.replace(' ','+') : book.title;
+    temp.querySelector('#amazonLink').parentElement.href = 'https://www.amazon.com/s?k=' + tempTitle + '+' + book.author.replace(' ', '+');
 
 
     temp.style.border = '5px solid goldenrod';
@@ -46,23 +51,70 @@ function setUpTemplate(book, template, bottomBorder) {
     return temp;
 }
 
-addBookToLibrary('The Hobbit','J.R.R. Tolkien',128,false);
-addBookToLibrary('Harry Potter and the Sorcerer\'s Stone','J.K. Rowling',128,true);
-addBookToLibrary('Harry Potter and the Sorcerer\'s Stone','J.K. Rowling',128,true);
-addBookToLibrary('Harry Potter and the Sorcerer\'s Stone','J.K. Rowling',128,true);
-addBookToLibrary('Harry Potter and the Sorcerer\'s Stone','J.K. Rowling',128,true);
-addBookToLibrary('Harry Potter and the Sorcerer\'s Stone','J.K. Rowling',128,true);
-addBookToLibrary('Harry Potter and the Sorcerer\'s Stone','J.K. Rowling',128,true);
-addBookToLibrary('Harry Potter and the Sorcerer\'s Stone','J.K. Rowling',128,true);
-addBookToLibrary('Harry Potter and the Sorcerer\'s Stone','J.K. Rowling',128,true);
+addBookToLibrary('The Hobbit','J.R.R. Tolkien',128, 'A small boy goes on a journey.',true);
+for(let i = 0; i<10; i++)
+    addBookToLibrary('Harry Potter and the Sorcerer\'s Stone','J.K. Rowling',128, 'A boy learns that he is destined to be a wizard.',Math.floor(Math.random()*2) == 1 ? true : false);
 render();
 
 function centerList() {
     bookList.style.marginLeft = (window.innerWidth-bookList.firstElementChild.clientWidth)/2 + 'px';
     titleBar.style.width = bookList.firstElementChild.clientWidth + 'px';
     titleBar.style.marginLeft = (window.innerWidth-titleBar.clientWidth)/2 + 'px';
+    menu.style.width = bookList.firstElementChild.clientWidth + 'px';
+    menu.style.marginLeft = (window.innerWidth-menu.clientWidth)/2 + 'px';
+    addBook.style.width = bookList.firstElementChild.clientWidth + 'px';
+    addBook.style.marginLeft = (window.innerWidth-addBook.clientWidth)/2 + 'px';
+}
+
+let menuOpen = (menu) => {
+    let srcSplit = body.querySelector(menu).firstElementChild.firstElementChild.src.split('/');
+    return srcSplit[srcSplit.length-1]=='menuClose.png'
+}
+
+function toggleMenu() {
+    let containsHash = this.href[this.href.length-1]=='#';
+    if(containsHash && menuOpen(`#add`)) body.querySelector('#add').firstElementChild.click();
+    this.firstElementChild.src = containsHash ? 'menuClose.png' : this.firstElementChild.src='menuOpen.png';
+    this.href = containsHash ? '#+' : '#';
+    menu.style.backgroundColor = containsHash ? 'goldenrod' : 'transparent';
+    menu.style.zIndex = containsHash ? 1 : 0;
+    menu.style.top = containsHash ? '100px' : '-400px';
+}
+
+function toggleAdd() {
+    let containsHash = this.href[this.href.length-1]=='#';
+    let x = menuOpen(`#hamburger`) && containsHash == true ? body.querySelector('#hamburger').firstElementChild.click() : false;
+    this.firstElementChild.src = containsHash ? 'menuClose.png' : 'plus.png';
+    this.href = containsHash ? '#+' : '#';
+    addBook.style.backgroundColor = containsHash ? 'goldenrod' : 'transparent';
+    addBook.style.zIndex = containsHash ? 1 : 0;
+    addBook.style.top = containsHash ? '100px' : '-400px';
+ 
+}
+
+function getFrom(form, name) {
+    return form.querySelector('input[name=\'' + name + '\']').type == 'text' ? form.querySelector('input[name=\'' + name + '\']').value : form.querySelector('input[name=\'' + name + '\']').checked;
+}
+
+function addBookForm(ech, edit) {
+    if(!edit) {
+        let form = body.querySelector('#addBook form')
+        addBookToLibrary(getFrom(form, 'title'), getFrom(form, 'author'), getFrom(form, 'pageNum'), getFrom(form, 'notes'), getFrom(form, 'read'))
+    }
+    const inputs = document.querySelectorAll('#addBook input');
+    for(index in inputs){
+        if(inputs[index].type=='text')
+            inputs[index].value = '';
+        else
+            inputs[index].checked = false;
+    }
+    body.querySelector('#add').firstElementChild.click();
+    render();
 }
 
 
 
 window.onresize = centerList;
+body.querySelector('#hamburger').firstElementChild.addEventListener('click', toggleMenu);
+body.querySelector('#add').firstElementChild.addEventListener('click', toggleAdd);
+body.querySelector('#addBook form input[type=\'submit\']').addEventListener('click', addBookForm, false)
